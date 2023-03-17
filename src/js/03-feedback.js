4ef1882afcb4989f90b484903482add4d4b270d1
Ikaruga666 <113863223+ArbalestARX7@users.noreply.github.com>
2023-03-17 12:23:12 +0200

    ДЗ 0.5

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,11 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
-const data = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+const data = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE)) || {};
 
 formFiller();
 
@@ -12,15 +13,16 @@ function onFormSubmit(evt) {
   evt.preventDefault();
   console.log(data);
   evt.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM_STATE);
 }
 
 function onFormInput(evt) {
   data[evt.target.name] = evt.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(data));
 }
 
 function formFiller() {
-  const filledData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const filledData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
 
   if (filledData) {
     form[0].value = filledData.email || '';
